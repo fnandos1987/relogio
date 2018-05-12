cf58d77de3e5e488b84954aded4df0e5968ffb81
Fernando Schwambach <fnandos1987@gmail.com>
2018-05-12 13:18:39 -0300

    relogio angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HorarioComponent } from './horario/horario.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorarioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/horario/horario.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horario',
+  templateUrl: './horario.component.html',
+  styleUrls: ['./horario.component.css']
+})
+export class HorarioComponent implements OnInit {
+
+  hora = '';
+
+  constructor() { }
+
+  ngOnInit() {
+    setInterval(() => this.mostrarhora(), 1000);
+  }
+
+  mostrarhora() {
+    const f = new Date();
+    this.hora = this.lpad(f.getHours()) + ':' + this.lpad(f.getMinutes()) + ':' + this.lpad(f.getSeconds());
+  }
+
+  lpad(num) {
+    if (num < 10) {
+      return '0' + num;
+    }
+    return num;
+  }
+}
